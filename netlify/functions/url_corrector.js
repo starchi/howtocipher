37f9d58414c2a52a11666043601771f9f0a7dde3
starchi <starchiwang@gmail.com>
2023-05-24 17:49:38 +0800

    Update url_corrector.js

--- netlify/functions/url_corrector.js
+++ netlify/functions/url_corrector.js
@@ -2,27 +2,11 @@ exports.handler = async (event) => {
   const { path } = event;
   const url = decodeURIComponent(path.replace(/^\/|\/$/g, ''));
 
-  if (!url) {
-    return {
-      statusCode: 400,
-      body: 'Missing URL',
-    };
-  }
-
-  let correctedUrl = url;
-
-  // 如果URL不包含'A'前缀，进行纠错处理
-  if (!url.includes('/191.html')) {
-    correctedUrl = 'https://howtocipher.netlify.app/beginner/A191.html'
-  }
-
-  console.log('Original URL:', url);
-  console.log('Corrected URL:', correctedUrl);
 
   return {
     statusCode: 302,
     headers: {
-      Location: correctedUrl,
+      Location: url,
     },
   };
 };
