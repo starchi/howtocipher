b92eb7fa956a5de7ad9a4ac14b42b23b260e4fd1
starchi <starchiwang@gmail.com>
2023-05-24 17:58:35 +0800

    Update url_corrector.js

--- netlify/functions/url_corrector.js
+++ netlify/functions/url_corrector.js
@@ -1,19 +1,27 @@
 exports.handler = async function(event, context) {
-  // 从查询参数中获取原始URL
-  const { url } = event.queryStringParameters;
+  try {
+    // 从 event.path 获取 URL 的路径
+    const path = event.path;
 
-  // 对原始URL进行重定向逻辑处理
-  let redirectUrl = url;
-  if (url.includes('/beginner/')) {
-    redirectUrl = url.replace('/beginner/', '/beginner/A');
+    // 对路径进行重定向逻辑处理
+    let redirectUrl = path;
+    if (path.includes('/beginner/')) {
+      redirectUrl = path.replace('/beginner/', '/beginner/A');
+    }
+
+    // 构建重定向响应
+    return {
+      statusCode: 301,
+      headers: {
+        Location: redirectUrl
+      },
+      body: ''
+    };
+  } catch (error) {
+    // 处理错误情况
+    return {
+      statusCode: 400,
+      body: error.message
+    };
   }
-
-  // 构建重定向响应
-  return {
-    statusCode: 301,
-    headers: {
-      Location: redirectUrl
-    },
-    body: ''
-  };
 };
