3b46dff00dfb6570cf3af2194d4a191b133dac43
starchi <starchiwang@gmail.com>
2023-05-24 16:54:51 +0800

    Update url_corrector.js
    simple test

--- netlify/functions/url_corrector.js
+++ netlify/functions/url_corrector.js
@@ -1,79 +1,13 @@
-function levenshteinDistance(s, t) {
-  const m = s.length;
-  const n = t.length;
-
-  if (m === 0) return n;
-  if (n === 0) return m;
-
-  const d = [];
-  for (let i = 0; i <= m; i++) {
-    d[i] = [i];
-  }
-  for (let j = 0; j <= n; j++) {
-    d[0][j] = j;
-  }
-
-  for (let j = 1; j <= n; j++) {
-    for (let i = 1; i <= m; i++) {
-      const cost = s[i - 1] === t[j - 1] ? 0 : 1;
-      d[i][j] = Math.min(
-        d[i - 1][j] + 1, // deletion
-        d[i][j - 1] + 1, // insertion
-        d[i - 1][j - 1] + cost // substitution
-      );
-    }
-  }
-
-  return d[m][n];
-}
-
-const fs = require('fs');
-const path = require('path');
-
 exports.handler = async (event) => {
   const { queryStringParameters } = event;
   const { url } = queryStringParameters;
 
-  // 获取当前目录下的文件列表
-  const files = fs.readdirSync(__dirname);
-  
-  if (!Array.isArray(files) || files.length === 0) {
-    // 文件列表为空或未定义
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'File list is empty or undefined' }),
-    };
-  }
+  const correctedUrl = url.replace(/\/(\d+)\.html$/, '/A$1.html');
 
-  
-  // 寻找编辑距离为1的页面
-  let correctedUrl = '';
-  for (const file of files) {
-    const filePath = path.join(__dirname, file);
-    const fileName = path.basename(filePath);
-    const distance = levenshteinDistance(url, fileName);
-    if (distance === 1) {
-      correctedUrl = file;
-      break;
-    }
-  }
-
-  // 返回页面
-  if (correctedUrl !== '') {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ correctedUrl }),
-    };
-  } else {
-    // 返回 common/wrong.html 页面
-    const wrongPageContent = `<html><body><h1>Wrong Page</h1></body></html>`;
-
-    return {
-      statusCode: 200,
-      body: wrongPageContent,
-      headers: {
-        'Content-Type': 'text/html',
-      },
-    };
-  }
+  return {
+    statusCode: 302,
+    headers: {
+      Location: correctedUrl,
+    },
+  };
 };
