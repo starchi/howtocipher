0effc807714a9bd27bab872dc23f3a9751434641
starchi <starchiwang@gmail.com>
2023-05-24 16:40:30 +0800

    Update url_corrector.js

--- netlify/functions/url_corrector.js
+++ netlify/functions/url_corrector.js
@@ -27,37 +27,6 @@ function levenshteinDistance(s, t) {
   return d[m][n];
 }
 
-exports.handler = async (event) => {
-  const { queryStringParameters } = event;
-  const { url } = queryStringParameters;
-
-  // 获取当前目录下的文件列表
-  const files = fs.readdirSync(__dirname);
-
-  // 寻找编辑距离为1的页面
-  
-
-  // 返回页面
-  if (correctedUrl !== '') {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ correctedUrl }),
-    };
-  } else {
-    // 返回 common/wrong.html 页面
-    const wrongPagePath = path.join(__dirname, 'common', 'wrong.html');
-    const wrongPageContent = fs.readFileSync(wrongPagePath, 'utf-8');
-
-    return {
-      statusCode: 200,
-      body: wrongPageContent,
-      headers: {
-        'Content-Type': 'text/html',
-      },
-    };
-  }
-};
-
 
 const path = require('path');
 
