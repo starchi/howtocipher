c3e024269b18ed3156a1d2417de2451bbdac2176
starchi <starchiwang@gmail.com>
2023-05-24 17:54:30 +0800

    Update url_corrector.js

--- netlify/functions/url_corrector.js
+++ netlify/functions/url_corrector.js
@@ -1,12 +1,19 @@
-exports.handler = async (event) => {
-  const { path } = event;
-  const url = decodeURIComponent(path.replace(/^\/|\/$/g, ''));
+exports.handler = async function(event, context) {
+  // 从查询参数中获取原始URL
+  const { url } = event.queryStringParameters;
 
+  // 对原始URL进行重定向逻辑处理
+  let redirectUrl = url;
+  if (url.includes('/beginner/')) {
+    redirectUrl = url.replace('/beginner/', '/beginner/A');
+  }
 
+  // 构建重定向响应
   return {
-    statusCode: 302,
+    statusCode: 301,
     headers: {
-      Location: url,
+      Location: redirectUrl
     },
+    body: ''
   };
 };
