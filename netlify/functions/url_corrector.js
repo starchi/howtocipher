a35b68ec0256bf271528267f7f74e6a06dc72205
starchi <starchiwang@gmail.com>
2023-05-24 17:03:46 +0800

    Update url_corrector.js

--- netlify/functions/url_corrector.js
+++ netlify/functions/url_corrector.js
@@ -1,6 +1,13 @@
 exports.handler = async (event) => {
-  const { queryStringParameters } = event;
-  const { url } = queryStringParameters;
+  const { path } = event;
+  const url = decodeURIComponent(path.replace(/^\/|\/$/g, ''));
+
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: 'Missing URL',
+    };
+  }
 
   const correctedUrl = url.replace(/\/(\d+)\.html$/, '/A$1.html');
 
